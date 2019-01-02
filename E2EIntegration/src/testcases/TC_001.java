1426fd0f1cbc57bbc8f6c6e5f24e7834a01e6c6c
Mehdi Hatami <mhatami@sequencecorp.com>
2019-01-01 21:54:23 -0500

    Created automation project

---
+++ E2EIntegration/src/testcases/TC_001.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_001 {
+
+	@Test
+	public void testcase() {
+		System.setProperty("webdriver.chrome.driver", "C:\\apps\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+	}
+}
